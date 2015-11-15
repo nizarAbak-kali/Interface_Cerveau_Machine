1797695304e5ee3aa2945106251a1e552713bc2e
nizarAbak-kali <nizarabakkali93@gmail.com>
2015-11-16 00:13:41 +0100

    Classe servant à representer toutes les caractéristique temporelle d'un neurones

---
+++ src/main/java/neuron/CaracteristiqueTemporel.java
@@ -0,0 +1,44 @@
+package neuron;
+
+/**
+ * Created by nizar on 15/11/2015.
+ *
+ * @author(Nizar ABAK-KALI)
+ * <p>
+ * class contenant les caracteristique temporel .
+ */
+public class CaracteristiqueTemporel {
+
+    double f1;
+    double f2;
+    double f3;
+    double f4;
+
+    public CaracteristiqueTemporel() {
+        init();
+    }
+
+    private void init() {
+        mean();
+        variance();
+        skewness();
+        kurtosis();
+    }
+
+    private void mean() {
+
+    }
+
+    private void variance() {
+
+    }
+
+    private void skewness() {
+
+    }
+
+    private void kurtosis() {
+
+    }
+
+}
