7ec27468751129cf72560bed240f52e836949109
nizarAbak-kali <nizarabakkali93@gmail.com>
2015-12-19 16:46:19 +0100

    mise à jour

---
+++ src/charts/Graphique.java
@@ -0,0 +1,20 @@
+package charts;/**
+ * Created by nizar on 11/11/2015.
+ *
+ * @author(Nizar ABAK-KALI)
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Graphique extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
