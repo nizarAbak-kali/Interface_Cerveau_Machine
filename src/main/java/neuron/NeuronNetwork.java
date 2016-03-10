60ea1033233606ea9556eefb8e5da65d6dc47628
Alexis Zurawska <zurawskacpgejj11@gmail.com>
2016-03-10 22:50:27 +0100

    Modification de la structure du reseau de neurones sous forme orientee objet

--- src/main/java/neuron/NeuronNetwork.java
+++ src/main/java/neuron/NeuronNetwork.java
@@ -1,90 +1,176 @@
 package neuron;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
- * Created by Alexou on 12/02/2016.
+ * Created by Alexou on 10/03/2016.
  */
 public class NeuronNetwork {
 
-    private float [][] neurons;
-    private int nbNeurons;
-    private int nbMinimalNeurons;
-    private int nbMaximalNeurons;
+    private ArrayList<Neuron> neurons;
+    private CaracteristiqueTemporel caracteristiquesTemporelles;
 
     public NeuronNetwork() {
-        Random r = new Random();
-        this.nbMinimalNeurons=10;
-        this.nbMaximalNeurons=100;
-        this.nbNeurons = nbMinimalNeurons + r.nextInt(nbMaximalNeurons - nbMinimalNeurons);
-        this.neurons = new float[nbNeurons][4];
+        this.neurons=new ArrayList<Neuron>();
+        this.neurons=remplirArrayList();
         init();
+        caracteristiquesTemporelles=new CaracteristiqueTemporel();
     }
 
     private void init() {
-        float minimalWeight = (float) (-2.4/this.nbNeurons);
-        float maximalWeight = (float) (2.4/this.nbNeurons);
+
+        float minimalWeight = (float) (-2.4/this.neurons.size());
+        float maximalWeight = (float) (2.4/this.neurons.size());
         Random r = new Random();
-        for(int i=0;i<neurons.length;i++) {
-            neurons[i][0]=0; //initialisation de la valeur des neurones à 0
-            neurons[i][1] = minimalWeight + (r.nextFloat()+ minimalWeight)-(maximalWeight); //initialisation du poids des neurones
-            neurons[i][2]=0; //initialisation des données pour chaque neurone
-            neurons[i][3]=0; //initialisation des gradients d'erreur
+
+        for(int i=0;i<this.neurons.size();i++) {
+            this.neurons.get(i).setWeight(minimalWeight + (r.nextFloat() + minimalWeight) - maximalWeight);
         }
+
     }
 
-    public void updateValues(int [] newValues) {
-        for(int i=0;i<neurons.length;i++) {
-            for(int j=0;j<newValues.length;j++) {
-                neurons[i][0]=newValues[j];
+    private ArrayList<Neuron> remplirArrayList() {
+        Random r = new Random();
+        int nbMinimalNeurons=10;
+        int nbMaximalNeurons=100;
+        int nbNeurons=nbMinimalNeurons + r.nextInt(nbMaximalNeurons - nbMinimalNeurons);
+
+        for(int i=0;i<=nbNeurons;i++) {
+            ajouterNeuron(new Neuron());
+        }
+
+        return this.neurons;
+    }
+
+    public ArrayList<Neuron> getNeurons() {
+        return this.neurons;
+    }
+
+    public CaracteristiqueTemporel getCaracteristiquesTemporelles() {
+        return this.caracteristiquesTemporelles;
+    }
+
+    public void setCaracteristiquesTemporelless(CaracteristiqueTemporel caracteristiquesTemporelle) {
+        this.caracteristiquesTemporelles=caracteristiquesTemporelle;
+    }
+
+    public void ajouterNeuron(Neuron neuron) {
+        this.neurons.add(neuron);
+    }
+
+    public void supprimerNeuron(Neuron neuron) {
+        for(int i=0;i<this.neurons.size();i++) {
+            if(this.neurons.get(i).equals(neuron)) {
+                this.neurons.remove(neuron);
             }
         }
     }
 
-    public void updateWeights(int [] changes) {
-        for(int i=0;i<neurons.length;i++) {
-            for(int j=0;j<changes.length;j++) {
-                neurons[i][1]=changes[j];
+    public void updateValues(float newValue) {
+        for (int i = 0; i < this.neurons.size(); i++) {
+            this.neurons.get(i).setValue(newValue);
+        }
+    }
+
+    public void updateValues(ArrayList<Float> newValues) throws LongueurDifferenteException {
+        if(this.neurons.size()==newValues.size()) {
+            for (int i = 0; i < this.neurons.size(); i++) {
+                for (int j = 0; j < newValues.size(); j++) {
+                    this.neurons.get(i).setValue(newValues.get(j));
+                }
+            }
+        }
+        else {
+            throw new LongueurDifferenteException();
+        }
+    }
+
+    public void updateWeights(float newWeight) {
+        for(int i=0;i<this.neurons.size();i++) {
+            this.neurons.get(i).setWeight(newWeight);
+        }
+    }
+
+    public void updateWeights(ArrayList<Float> newWeights) throws LongueurDifferenteException {
+        if(this.neurons.size()==newWeights.size()) {
+            for(int i=0;i<this.neurons.size();i++) {
+                for(int j=0;j<newWeights.size();j++) {
+                    this.neurons.get(i).setWeight(newWeights.get(j));
+                }
+            }
+        }
+        else {
+            throw new LongueurDifferenteException();
+        }
+    }
+
+    public void updateDonnees(float newDonnee) {
+        for(int i=0;i<this.neurons.size();i++) {
+            this.neurons.get(i).setDonnee(newDonnee);
+        }
+    }
+
+    public void updateDonnees(ArrayList<Float> newDonnees) throws LongueurDifferenteException {
+        if(this.neurons.size()==newDonnees.size()) {
+            for(int i=0;i<this.neurons.size();i++) {
+                for(int j=0;j<newDonnees.size();j++) {
+                    this.neurons.get(i).setDonnee(newDonnees.get(j));
+                }
+            }
+        }
+        else {
+            throw new LongueurDifferenteException();
+        }
+    }
+
+    public void updateErrorGradients(float newErrorGradient) {
+        for(int i=0;i<this.neurons.size();i++) {
+            this.neurons.get(i).setErrorGradient(newErrorGradient);
+        }
+    }
+
+    public void updateErrorGradients(ArrayList<Float> newErrorGradients) throws LongueurDifferenteException {
+        if(this.neurons.size()==newErrorGradients.size()) {
+            for(int i=0;i<this.neurons.size();i++) {
+                for(int j=0;j<newErrorGradients.size();j++) {
+                    this.neurons.get(i).setErrorGradient(newErrorGradients.get(j));
+                }
+            }
+        }
+        else {
+            throw new LongueurDifferenteException();
+        }
+    }
+
+    public void updateValueNeuron(int neuron, float newValue) {
+        for(int i=0;i<this.neurons.size();i++) {
+            if(this.neurons.get(i).equals(neuron)) {
+                this.neurons.get(i).setValue(newValue);
             }
         }
     }
 
-    public void updateDatas(int [] datas) {
-        for(int i=0;i<neurons.length;i++) {
-            for(int j=0;j<datas.length;j++) {
-                neurons[i][2]=datas[j];
+    public void updateWeightNeuron(int neuron, float newWeight) {
+        for(int i=0;i<this.neurons.size();i++) {
+            if(this.neurons.get(i).equals(neuron)) {
+                this.neurons.get(i).setWeight(newWeight);
             }
         }
     }
 
-    public void updateErrorsGradients(int [] newErrorsGradients) {
-        for(int i=0;i<neurons.length;i++) {
-            for(int j=0;j<newErrorsGradients.length;j++) {
-                neurons[i][3]=newErrorsGradients[j];
+    public void updateDonneeNeuron(int neuron, float newDonnee) {
+        for(int i=0;i<this.neurons.size();i++) {
+            if(this.neurons.get(i).equals(neuron)) {
+                this.neurons.get(i).setDonnee(newDonnee);
             }
         }
     }
 
-    public void updateErrorGradientNeuron(int newErrorGradient, int neuron) {
-        for(int i=0;i<neurons.length;i++) {
-            if(neuron==i) {
-
-            }
-        }
-    }
-
-    public void updateWeightNeuron(int newWeight, int neuron) {
-        for(int i=0;i<neurons.length;i++) {
-            if(neuron==i) {
-                neurons[i][1]=newWeight;
-            }
-        }
-    }
-
-    public void changeValueNeuron(int value, int neuron) {
-        for(int i=0;i<neurons.length;i++) {
-            if(neuron==i) {
-                neurons[i][0]=value;
+    public void updateErrorGradientNeuron(int neuron, float newErrorGradient) {
+        for(int i=0;i<this.neurons.size();i++) {
+            if(this.neurons.get(i).equals(neuron)) {
+                this.neurons.get(i).setErrorGradient(newErrorGradient);
             }
         }
     }
