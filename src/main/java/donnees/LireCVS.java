461137ff891d7dc4f57d6afc9242449afe77817b
Alexis Zurawska <zurawskacpgejj11@gmail.com>
2016-05-31 18:59:41 +0200

    Modification de la taille du tableau en fonction du nombre de floats par ligne + contexte rapport complete

--- src/main/java/donnees/LireCVS.java
+++ src/main/java/donnees/LireCVS.java
@@ -20,59 +20,6 @@ public class LireCVS {
     }
 
     /**
-     * Méthode permettant de charger en mémoire le fichier passé en paramètre.
-     * @return Un tableau de tableau de String représentant les trois signaux du fichier
-     */
-
-    public String[][] chargerFichier() {
-
-        String[][] listeDonnees = new String[3][1000];
-        File fichier = new File(file_p);
-        String[] tabIntermediaire = new String[1000];
-
-        try {
-            InputStream flux = new FileInputStream(fichier);
-            InputStreamReader lecture = new InputStreamReader(flux);
-            BufferedReader buff = new BufferedReader(lecture);
-            String ligne;
-            int i=0;
-
-            while((ligne=buff.readLine())!=null && i!=listeDonnees.length) {
-                tabIntermediaire=ligne.split(",");
-                listeDonnees[i]=tabIntermediaire;
-                i++;
-            }
-            buff.close();
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-
-        return listeDonnees;
-
-    }
-
-    /**
-     * Méthode permettant de convertir les données de type String en float (ou nombres flottants)
-     * @param donnees
-     * @return Le tableau de données converties.
-     */
-
-    public float[][] convertirDonnees(String[][] donnees) {
-
-        float[][] donneesConverties = new float[3][1000];
-
-        for(int i=0;i<donnees.length;i++) {
-            for(int j=0;j<donnees[i].length;j++) {
-                donneesConverties[i][j]=Float.valueOf(donnees[i][j]);
-            }
-        }
-
-        return donneesConverties;
-
-    }
-
-    /**
      * Charge la ligne du fichier dont le numéro est passé en paramètre.
      * @param numLigne
      * @return
@@ -81,7 +28,7 @@ public class LireCVS {
     public String[] chargerLingneFichier(int numLigne) {
         FileReader monFichier = null;
         BufferedReader tampon = null;
-        String[] tabIntermediaire = new String[1000];
+        String[] tabIntermediaire = new String[2560];
 
         try {
             monFichier = new FileReader(file_p);
